85e6021824aa0349acc586904ee0ed936e1cd695
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-10-19 16:35:49 -0400

    feat(cli): Add node name and node image as flags for `start`

--- fishtank-cli/src/commands/start.ts
+++ fishtank-cli/src/commands/start.ts
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Command, Config, Flags } from '@oclif/core'
-import { Cluster } from 'fishtank'
+import { BootstrapOptions, Cluster } from 'fishtank'
 
 export abstract class Start extends Command {
   static description = 'Spin up a new cluster'
@@ -16,10 +16,22 @@ export abstract class Start extends Command {
   ]
 
   static flags = {
-    noBootstrap: Flags.boolean({
+    bootstrap: Flags.boolean({
       char: 'B',
       description:
         'Do not start any bootstrap node and do not mine any block during the creation of the cluster',
+      default: true,
+      allowNo: true,
+    }),
+    nodeName: Flags.string({
+      char: 'n',
+      description: 'Bootstrap node name',
+      parse: (input: string) => Promise.resolve(input.trim()),
+    }),
+    nodeImage: Flags.string({
+      char: 'i',
+      description: 'Bootstrap node image',
+      parse: (input: string) => Promise.resolve(input.trim()),
     }),
   }
 
@@ -31,7 +43,15 @@ export abstract class Start extends Command {
     const { args, flags } = await this.parse(Start)
     const clusterName = args.name as string
     const cluster = new Cluster({ name: clusterName })
-    await cluster.init({ bootstrap: !flags.noBootstrap })
+
+    if (flags.nodeName !== undefined && flags.nodeImage !== undefined) {
+      await cluster.init({
+        bootstrap: { nodeImage: flags.nodeImage, nodeName: flags.nodeName },
+      })
+    } else {
+      await cluster.init({ bootstrap: flags.bootstrap })
+    }
+
     this.exit(0)
   }
 }
