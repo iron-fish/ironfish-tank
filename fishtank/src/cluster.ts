82d0b77b47f1b46c08b920513263ee2fd3272711
Hugh Cunningham <hugh@iflabs.network>
2024-04-24 14:28:15 -0700

    spawns nodes with IPC disabled by default
    avoids permissions issues listening on IPC socket:
    
    ```
    Error: listen EPERM: operation not permitted /root/.ironfish/ironfish.ipc
    ```
    
    rpc connections use TCP instead of IPC in fishtank, so IPC does not need to be
    enabled by default

--- fishtank/src/cluster.ts
+++ fishtank/src/cluster.ts
@@ -131,6 +131,7 @@ export class Cluster {
     const config = structuredClone(options.config) || {}
     config.enableRpcTcp ??= true
     config.enableRpcTls ??= false
+    config.enableRpcIpc ??= false
     config.rpcTcpHost ??= ''
     config.preemptiveBlockMining ??= false
 
